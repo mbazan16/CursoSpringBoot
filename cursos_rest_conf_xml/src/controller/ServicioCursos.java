ece623eb92f52e638c46bbf696f38980d3877612
MARIA <MARIA@W-PORSCHE>
2020-05-26 12:48:03 +0200

    Ejemplo de proyecto Spring configurado por xml

---
+++ cursos_rest_conf_xml/src/controller/ServicioCursos.java
@@ -0,0 +1,51 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import dao.DaoCursos;
+import model.Curso;
+
+@RestController
+public class ServicioCursos {
+	@Autowired
+	DaoCursos daoCursos;
+	
+	@GetMapping(value="cursos",produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Curso> getCursos(){
+		return daoCursos.recuperarTodosCursos();
+	}
+	@GetMapping(value="cursos/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+	public Curso buscarCurso(@PathVariable("id") int idCurso) {
+		return daoCursos.buscarCurso(idCurso);
+	}
+	
+	@DeleteMapping(value="cursos/{id}")
+	public void eliminarCurso(@PathVariable("id") int idCurso) {
+		daoCursos.eliminarCurso(idCurso);
+	}
+	@PostMapping(value="cursos",consumes=MediaType.APPLICATION_JSON_VALUE)
+	public void nuevoCurso(@RequestBody Curso curso) {
+		daoCursos.altaCurso(curso);
+	}
+	
+	@PutMapping(value="cursos/{id}/{nuevoNombre}")
+	public void actualizarCurso(@PathVariable("id") int idCurso,
+			@PathVariable("nuevoNombre") String nombre) {
+		Curso curso=daoCursos.buscarCurso(idCurso);
+		if(curso!=null) {
+			curso.setDenominacion(nombre);
+			daoCursos.actualizarCurso(curso);
+		}
+	}
+	
+}
---
+++ cursos_rest_conf_xml/src/model/Curso.java
@@ -0,0 +1,64 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the cursos database table.
+ * 
+ */
+@Entity
+@Table(name="cursos")
+@NamedQuery(name="Curso.findAll", query="SELECT c FROM Curso c")
+public class Curso implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idCurso;
+	
+	private String denominacion;
+	
+	private int duracion;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date fechaInicio;
+
+	public Curso() {
+	}
+
+	public int getIdCurso() {
+		return this.idCurso;
+	}
+
+	public void setIdCurso(int idCurso) {
+		this.idCurso = idCurso;
+	}
+
+	public String getDenominacion() {
+		return this.denominacion;
+	}
+
+	public void setDenominacion(String denominacion) {
+		this.denominacion = denominacion;
+	}
+
+	public int getDuracion() {
+		return this.duracion;
+	}
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+	public Date getFechaInicio() {
+		return this.fechaInicio;
+	}
+
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+}
