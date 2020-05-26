650ece7093cafbddb19172af9357a2182ebb3990
MARIA <MARIA@W-PORSCHE>
2020-05-26 10:26:32 +0200

    Demos proyectos Spring Boot basicos

---
+++ demoDWP/src/com/example/demo/controller/SaludoBoot.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SaludoBoot {
+	
+	@GetMapping(value="saludo",produces=MediaType.TEXT_PLAIN_VALUE)
+	public String saludar() {
+		return "Mi saludo";
+	}
+
+}
