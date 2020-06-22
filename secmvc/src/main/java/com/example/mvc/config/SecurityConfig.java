ed31c2c5b0c9aabcaa9cc811fd6fb41b67cd21cf
MARIA <MARIA@W-PORSCHE>
2020-06-22 09:26:20 +0200

    Ejemplo de proyecto MVC Securizado. Global y metodos

--- secmvc/src/main/java/com/example/mvc/config/SecurityConfig.java
+++ secmvc/src/main/java/com/example/mvc/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.mvc.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,14 +9,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	CustomAccessDeniedHandler customAccessDeniedHandler;
 	
 	@Bean
 	public AuthenticationManager authMg() throws Exception {
@@ -62,7 +62,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		.and()
 		.exceptionHandling()
 		.accessDeniedPage("/accessDenied")
-		.accessDeniedHandler(new CustomAccessDeniedHandler());
+		.accessDeniedHandler(customAccessDeniedHandler);
 
 	}
 	
